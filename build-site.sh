22b10fbdbf29f723c331da2b0d345d8e6d68eb0f
Sicheng <sichengzhangcn@gmail.com>
2022-08-24 15:00:24 -0500

    update: build-site script

--- build-site.sh
+++ build-site.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
 tiddlywiki --rendertiddlers [!is[system]] $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain --rendertiddler $:/core/templates/alltiddlers.template.html static/archive.html text/plain
